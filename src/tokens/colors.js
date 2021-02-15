7101cb612496dd96a452202eab5aa03d11bc996d
Ruan Ramalho <ruanmbramalho@gmail.com>
2021-02-15 17:15:12 -0300

    first commit

--- src/tokens/colors.js
+++ src/tokens/colors.js
@@ -1,9 +1,17 @@
 const white = '#FFFFFF';
 const dark = '#000000';
+const darkGreen = '#6B8067';
+const green = '#61CB46';
+const gray = '#F6F6F6';
+const darkGray = '#F7F7F7';
 
 const colors = {
   white,
   dark,
+  darkGreen,
+  green,
+  gray,
+  darkGray,
 };
 
 export default colors;
