2ae18fa54d4e56044872cae7173920777c85a37b
Ruan Ramalho <ruanmbramalho@gmail.com>
2021-02-18 00:08:09 -0300

    feat(Store): add fetch api

--- src/pages/Store/index.js
+++ src/pages/Store/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Sneaker from '../../components/Sneaker';
 import Header from '../../components/Header';
 
-const Store = () => (
-  <>
-    <Header />
-    <Sneaker />
-  </>
-);
+const Store = () => {
+  const [items, setItems] = useState([]);
+
+  const loadProducts = async () => {
+    try {
+      const response = await fetch(
+        'http://voliveira.s3-sa-east-1.amazonaws.com/sneakers/index.json',
+      );
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Sneaker />
+      {console.log(items)};
+    </>
+  );
+};
 
 export default Store;
